2927b2b112bb441c0fb1dd41525bdaf988f99c04
TrangJonathan <jonathan93113@outlook.com>
2019-08-05 20:42:15 +0700

    initial

---
+++ client/build/precache-manifest.e3dc2707bf94c06d2091e51d0353450d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e675305bb6dddbfd8493dbb1c233d312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9bd33b2e09181a082f8",
+    "url": "/static/css/2.0f801562.chunk.css"
+  },
+  {
+    "revision": "94198d9c5d73af3f008c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a9bd33b2e09181a082f8",
+    "url": "/static/js/2.9ed8200d.chunk.js"
+  },
+  {
+    "revision": "94198d9c5d73af3f008c",
+    "url": "/static/js/main.9dc88d1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb://localhost:27017/googlebooks" ,
+    // mongoURI: "mongodb+srv://Jonathan:ajspasswordismine113@cluster0-5th5q.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+  };
